d2e52929cbeaf15f3bc438923e777db660c70a23
MsNiyaJ <shaniyasmalcolm@gmail.com>
2021-09-29 16:25:42 -0400

    got user data from the gtihub api and displayed them

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  }
+  
+  // Runs on load
+  async componentDidMount() {
+    this.setState({loading: true});
+
+    const res = await axios.get('https://api.github.com/users');
+
+    res.data.push({
+      login: 'MsNiyaJ',
+      id: 47842684,
+      avatar_url: 'https://avatars.githubusercontent.com/u/47842684?v=4',
+      html_url: 'https://github.com/MsNiyaJ'
+    })
+
+    this.setState({users: res.data, loading: false});
+  }
+
   render() {
-    
     return (
       <div className='App'>
         <Navbar/>
         <div className='container'>
-          <Users/>
+          <Users loading={this.state.loading} users={this.state.users}/>
         </div>
       </div>
     );
