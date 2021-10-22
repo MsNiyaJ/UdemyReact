580672a6471cb7cb0b15e8e44c315f1d47bd0f81
MsNiyaJ <shaniyasmalcolm@gmail.com>
2021-10-22 14:19:42 -0400

    removed types that were unused

--- contact-keeper/client/src/context/contact/ContactState.js
+++ contact-keeper/client/src/context/contact/ContactState.js
@@ -3,14 +3,12 @@ import { v4 as uuid } from 'uuid';
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
 import {
-  GET_CONTACTS,
   ADD_CONTACT,
   DELETE_CONTACT,
   SET_CURRENT,
   CLEAR_CURRENT,
   UPDATE_CONTACT,
   FILTER_CONTACTS,
-  CLEAR_CONTACTS,
   CLEAR_FILTER,
 } from '../types';
 
