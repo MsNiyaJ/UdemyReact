f25873ba9ff4359539f0cb6352fd72b7d9649a3c
MsNiyaJ <shaniyasmalcolm@gmail.com>
2021-10-25 11:41:19 -0400

    contacts state is cleared on logout

--- contact-keeper/client/src/context/contact/ContactState.js
+++ contact-keeper/client/src/context/contact/ContactState.js
@@ -64,6 +64,11 @@ const ContactState = (props) => {
     dispatch({ type: DELETE_CONTACT, payload: id });
   };
 
+  // Clear Contacts
+  const clearContacts = () => {
+    dispatch({ type: CLEAR_CONTACTS });
+  };
+
   // Set Current Contact
   const setCurrent = (contact) => {
     dispatch({ type: SET_CURRENT, payload: contact });
@@ -104,6 +109,7 @@ const ContactState = (props) => {
         filterContacts,
         clearFilter,
         getContacts,
+        clearContacts,
       }}
     >
       {props.children}
